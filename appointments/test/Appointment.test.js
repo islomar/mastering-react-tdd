bc4ff26bdc5d72ce691b9e3fcd03694b4015cf0c
Isidro Lopez <islomar@gmail.com>
2020-07-11 21:22:48 +0200

    [appointments] [test] AppointmsDayView renders each appointment in an li

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -56,4 +56,21 @@ describe("AppointmentsDayView", () => {
       container.querySelector("ol").children
     ).toHaveLength(2);
   });
+
+  it("renders each appointment in an li", () => {
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) }
+    ];
+    render(<AppointmentsDayView appointments={ appointments }/>);
+
+    expect(container.querySelectorAll("li")).toHaveLength(2);
+    expect(
+      container.querySelectorAll('li')[0].textContent
+    ).toEqual('12:00');
+    expect(
+      container.querySelectorAll('li')[1].textContent
+    ).toEqual('13:00');
+  });
 });
